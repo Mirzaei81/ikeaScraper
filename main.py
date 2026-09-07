edd2ae4bc966463d778c8d1d371b10e36af720a1
Your Name <you@example.com>
2026-09-07 12:02:16 +0330

    report after finish

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import asyncio
 from dotenv import load_dotenv
 load_dotenv()
 from Ikea.ikea_parser import getProductDetail,init as ikeaInit
-from Ikea.zardan import updateItem,getItems,init as zardanInit,dispose
+from Ikea.zardan import updateItem,getItems,init as zardanInit,dispose,root
 async def main():
     i=0 
     await zardanInit()
     await ikeaInit()
+    c = 0
+    t = 0 
     try:
         async for  i in getItems():
             price,tag,stock= await getProductDetail(i)
+            t+=1
             if price:
+                c +=1
                 await updateItem(i,price,stock,tag)
+
+        print(f"ended the session with {c}/{t}")
+        root.info(f"ended the session with {c}/{t}")
     except Exception as e:
         print(e)
 async def runner():
